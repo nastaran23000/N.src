ff54fd2445322ab2aa6d2d2ba495f9d45f6806b0
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-07 20:33:59 +0200

    completed.

--- nastaran_tasks/crud/index.php
+++ nastaran_tasks/crud/index.php
@@ -28,4 +28,5 @@ include 'header.php' ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+    
     <?php include 'footer.php' ?>
